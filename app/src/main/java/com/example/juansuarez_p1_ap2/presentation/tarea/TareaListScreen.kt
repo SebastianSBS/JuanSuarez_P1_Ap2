46a3c507b5763ad080283fba70511cef9f5aaf80
SebastianSBS <Juan_Suarez1@ucne.edu.do>
2025-06-04 19:48:06 -0400

    Correccion de valores.

--- app/src/main/java/com/example/juansuarez_p1_ap2/presentation/tarea/TareaListScreen.kt
+++ app/src/main/java/com/example/juansuarez_p1_ap2/presentation/tarea/TareaListScreen.kt
@@ -1,11 +1,3 @@
 package com.example.juansuarez_p1_ap2.presentation.tarea
 
-import com.example.juansuarez_p1_ap2.data.local.entities.TareaEntity
 
-data class TareaUiState(
-    val tareaId: Int? = null,
-    val descripcion: String? = "",
-    val tiempo: Int? = null,
-    val errorMessage: String? = null,
-    val tareas: List<TareaEntity> = emptyList()
-)
