98257cedb69ba6c69922714a7629391568c53ec9
SebastianSBS <Juan_Suarez1@ucne.edu.do>
2025-06-04 19:05:48 -0400

    TareaDb creada y definida. (Todo lo que he estado creando lo defino tambien, se me olvido mencionar)

---
+++ app/src/main/java/com/example/juansuarez_p1_ap2/data/local/database/tareadb/TareaDb.kt
@@ -0,0 +1,16 @@
+package com.example.juansuarez_p1_ap2.data.local.database.tareadb
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.juansuarez_p1_ap2.data.local.dao.TareaDao
+import com.example.juansuarez_p1_ap2.data.local.entities.TareaEntity
+
+@Database(
+    entities = [
+        TareaEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class TareaDb : RoomDatabase() {
+    abstract fun TareaDao(): TareaDao}
