4c019d625624ac5b5315b148ca6ada864efb9906
SebastianSBS <Juan_Suarez1@ucne.edu.do>
2025-06-04 19:43:41 -0400

    TareaListScreen definida.

--- app/src/main/java/com/example/juansuarez_p1_ap2/presentation/tarea/TareaListScreen.kt
+++ app/src/main/java/com/example/juansuarez_p1_ap2/presentation/tarea/TareaListScreen.kt
@@ -1,2 +1,11 @@
 package com.example.juansuarez_p1_ap2.presentation.tarea
 
+import com.example.juansuarez_p1_ap2.data.local.entities.TareaEntity
+
+data class TareaUiState(
+    val tareaId: Int? = null,
+    val descripcion: String? = "",
+    val tiempo: Int? = null,
+    val errorMessage: String? = null,
+    val tareas: List<TareaEntity> = emptyList()
+)
